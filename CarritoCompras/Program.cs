3726230ed68e838c3f43dbe041d89c7a62a4c57b
Agus ;) <agustinacaffaratti4@gmail.com>
2025-06-10 15:54:36 -0300

    Update Program.cs

--- CarritoCompras/Program.cs
+++ CarritoCompras/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CarritoCompras
 {
@@ -11,8 +11,30 @@ namespace CarritoCompras
             Console.WriteLine("Bienvenido a la tienda online de La Panaderia Feliz");
             Console.WriteLine("\n        Qué quiere hacer hoy?");
             Console.WriteLine("\n1- Ver Catalogo \n2- Ver su carrito \n3-Ver categorias de productos \n4-Salir");
-            int accion = Console.ReadLine("Ingrese el numero de lo que desea hacer: ");
-            Console.WriteLine(accion);
+            string input = Console.ReadLine();
+            int accion = int.Parse(input);
+
+            switch (accion)
+            {
+                case 1:
+                    Console.WriteLine("pusiste 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("pusiste 2");
+                    break;
+
+                case 3:
+                    Console.WriteLine("pusiste 3");
+                    break;
+
+                case 4:
+                    Console.WriteLine("pusiste 4");
+                    break;
+
+                default:
+                    Console.WriteLine("Numero no valido");
+            }
         }
     }
 }

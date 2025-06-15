4a1057e00f189b9a26bc2f7bbfcc07fa295bb70c
Luli2784 <lulimbueno@gmail.com>
2025-06-15 16:16:56 -0300

    Metodos Agregar y eliminar

--- CarritoCompras/Carrito.cs
+++ CarritoCompras/Carrito.cs
@@ -14,5 +14,34 @@ namespace CarritoCompras
         {
 
         }
+        public void Agregar(int cod, int cant)
+        {
+            int i = 0;
+            for (i = 0; i < items.Count; i++)
+            {
+                if (cod == items[i].Producto.Codigo)
+                {
+                    items[i].Cantidad += cant;
+                    i = items.Count + 1;
+                }
+            }
+            if (i == items.Count)
+            {
+                items[i + 1].Cantidad = cant;
+                items[i + 1].Producto.Codigo = cod;
+            }
+        }
+
+        public void Eliminar(int cod)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (cod == items[i].Producto.Codigo)
+                {
+                    items.RemoveAt(i);
+                    i = items.Count;
+                }
+            }
+        }
     }
 }
